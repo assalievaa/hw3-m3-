4b01c86f8c07bf119ac6bf80abe5d49f139902db
assalievaa <122771137+assalievaa@users.noreply.github.com>
2024-12-16 20:05:09 +0600

    Add files via upload

---
+++ new/new/js/home_works.js
@@ -0,0 +1,81 @@
+// // GMAIL BLOCK
+//
+// const gmailInput = document.querySelector('#gmail_input')
+// const gmailButton = document.querySelector('#gmail_button')
+// const gmailResult = document.querySelector('#gmail_result')
+//
+// const redExp = /^[a-z0-9._-]+@gmail\.com$/i
+//
+// gmailButton.onclick = () => {
+//     if (redExp.test(gmailInput.value)) {
+//         gmailResult.style.color = 'green'
+//         gmailResult.innerHTML = 'OK'
+//     }else {
+//         gmailResult.style.color = 'red'
+//         gmailResult.innerHTML = 'invalid gmail'
+//     }
+// }
+//
+//
+// //MOVE BLOCK
+//
+const child_block = document.querySelector('.child_block')
+const parent_block = document.querySelector('.parent_block')
+
+let num = 0;
+let num_2 = 0;
+
+const maxWidth = parent_block.offsetWidth - child_block.offsetWidth
+const maxHeight = parent_block.offsetHeight -child_block.offsetHeight
+const square = () => {
+    if (num < maxWidth && num_2<=0) {
+        num++
+        child_block.style.left = `${num}px`
+        requestAnimationFrame(square)
+    }else if (num >= maxWidth && num_2 < maxHeight ) {
+        num_2++
+        child_block.style.top = `${num_2}px`
+        requestAnimationFrame(square)
+    }else if (num > 0  && num_2 >= maxHeight) {
+        num--
+        child_block.style.left = `${num}px`
+        requestAnimationFrame(square)
+    }else if(num >= 0 && num_2>0){
+        num_2--
+        child_block.style.top = `${num_2}px`
+        requestAnimationFrame(square)
+
+    }
+}
+square()
+
+const zero = document.querySelector('#seconds')
+const start_btn = document.querySelector('#start')
+const stop_btn = document.querySelector('#stop')
+const reset_btn = document.querySelector('#reset')
+
+let interval
+let count = 0
+
+start_btn.onclick = () => {
+    if (!interval) {
+        interval = setInterval(() => {
+            count++
+            zero.innerHTML = count
+        }, 1000)
+    }
+}
+
+stop_btn.onclick = () => {
+    clearInterval(interval)
+    interval = null
+}
+
+reset_btn.onclick = () => {
+    clearInterval(interval)
+    interval = null
+    count = 0
+    zero.innerHTML = count
+}
+
+
